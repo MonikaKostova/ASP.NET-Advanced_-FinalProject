7fd383356d6b6578356dbef27ceba95f2d0d1b6c
MonikaKostova <137040970+MonikaKostova@users.noreply.github.com>
2024-11-18 16:29:54 +0200

    updating ViewModels

--- CalorieTrackerApp/Models/RecipeImageViewModel.cs
+++ CalorieTrackerApp/Models/RecipeImageViewModel.cs
@@ -2,5 +2,10 @@
 {
     public class RecipeImageViewModel
     {
+        public int Id { get; set; }
+        public string? Url { get; set; }
+
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; } = null!; // To display recipe name
     }
 }
--- CalorieTrackerApp/Models/RecipeViewModel.cs
+++ CalorieTrackerApp/Models/RecipeViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace CalorieTrackerCookBookApp.Models
-{
+﻿namespace CalorieTrackerCookBookApp.Models;
+
+using System.ComponentModel.DataAnnotations;
+using static CalorieTrackerCookBookApp.Common.ValidationConstraints;
+
     public class RecipeViewModel
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string RecipeName { get; set; } = null!;
+
+        [Required]
+        public string RecipeDescription { get; set; } = null!;
+
+        [Required]
+        public string Category { get; set; } = null!;
+
+        public string OwnerId { get; set; } = null!;
+        public string OwnerName { get; set; } = null!; // To display the owner's name
+
+        public List<IngredientViewModel> Ingredients { get; set; } = new List<IngredientViewModel>();
+        public List<CommentViewModel> Comments { get; set; } = new List<CommentViewModel>();
     }
-}
+
