8e1db8b4ac13cd82727d553cf4b9910da95bc893
MonikaKostova <137040970+MonikaKostova@users.noreply.github.com>
2024-11-18 17:04:49 +0200

    adding Controllers

---
+++ CalorieTrackerApp/Controllers/FavoriteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalorieTrackerCookBookApp.Controllers
+{
+    public class FavoriteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
