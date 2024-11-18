fdddbb665127946ca023b42373e5cea725fa78d9
MonikaKostova <137040970+MonikaKostova@users.noreply.github.com>
2024-11-18 16:58:57 +0200

    adding Controller nad Interface

---
+++ CalorieTrackerApp/Controllers/IRecipeService.cs
@@ -0,0 +1,14 @@
+﻿using CalorieTrackerCookBookApp.Models;
+
+namespace CalorieTrackerCookBookApp.Controllers
+{
+    public interface IRecipeService
+    {
+        IEnumerable<RecipeViewModel> GetAllRecipes();
+        RecipeViewModel GetRecipeById(int id);
+        RecipeEditViewModel GetRecipeForEdit(int id);
+        void CreateRecipe(RecipeCreateViewModel model);
+        void UpdateRecipe(RecipeEditViewModel model);
+        void DeleteRecipe(int id);
+    }
+}
---
+++ CalorieTrackerApp/Controllers/RecipeController.cs
@@ -0,0 +1,76 @@
+﻿using CalorieTrackerCookBookApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalorieTrackerCookBookApp.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly IRecipeService _recipeService;
+
+        public RecipeController(IRecipeService recipeService)
+        {
+            _recipeService = recipeService;
+        }
+
+        public IActionResult Index()
+        {
+            var recipes = _recipeService.GetAllRecipes();
+            return View(recipes);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var recipe = _recipeService.GetRecipeById(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            return View(recipe);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(RecipeCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _recipeService.CreateRecipe(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recipe = _recipeService.GetRecipeForEdit(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            return View(recipe);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(RecipeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _recipeService.UpdateRecipe(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _recipeService.DeleteRecipe(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
