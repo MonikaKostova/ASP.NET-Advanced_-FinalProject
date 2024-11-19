4e3f8afb8c13decb433519d7a13434c6eea78173
MonikaKostova <137040970+MonikaKostova@users.noreply.github.com>
2024-11-19 11:35:13 +0200

    updating Controller and Interface

--- CalorieTrackerApp/Controllers/CommentController.cs
+++ CalorieTrackerApp/Controllers/CommentController.cs
@@ -1,12 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CalorieTrackerCookBookApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CalorieTrackerCookBookApp.Controllers
 {
     public class CommentController : Controller
     {
-        public IActionResult Index()
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
         {
-            return View();
+            _commentService = commentService;
+        }
+
+        public IActionResult Index(int recipeId)
+        {
+            var comments = _commentService.GetCommentsForRecipe(recipeId);
+            return View(comments);
+        }
+
+        [HttpGet]
+        public IActionResult Create(int recipeId)
+        {
+            return View(new CommentCreateViewModel { RecipeId = recipeId });
+        }
+
+        [HttpPost]
+        public IActionResult Create(CommentCreateViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                _commentService.CreateComment(model);
+                return RedirectToAction("Index", new { recipeId = model.RecipeId });
+            }
+            return View(model);
+        }
+
+        public IActionResult Approve(int id)
+        {
+            _commentService.ApproveComment(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _commentService.DeleteComment(id);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ CalorieTrackerApp/Controllers/ICommentService.cs
@@ -0,0 +1,16 @@
+﻿using CalorieTrackerCookBookApp.Models;
+
+namespace CalorieTrackerCookBookApp.Controllers
+{
+    public interface ICommentService
+    {
+        IEnumerable<CommentViewModel> GetAllComments();
+        CommentViewModel GetCommentById(int id);
+        IEnumerable<CommentViewModel> GetCommentsForRecipe(int recipeId); 
+        void CreateComment(CommentCreateViewModel model);
+        CommentViewModel GetCommentForEdit(int id);
+        void UpdateComment(CommentViewModel model);
+        void DeleteComment(int id);
+        void ApproveComment(int commentId); 
+    }
+}
